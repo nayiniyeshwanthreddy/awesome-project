ce26de0b39eb7e4aa84f5e648a2f8db658aa7daf
YESHWANTH REDDY NAYINI <nayiniyeshwanthreddy@gmail.com>
2021-11-11 09:16:41 -0500

    second day changes till header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,20 @@ import ReactDOM from 'react-dom';
 function Header(){
     return (<header>
       <h1>Letz Share</h1>
+ 
+ <table className="header-table">
+     <tr>
+         <td>
+             About Us
+         </td>
+         <td>
+             Contact Us
+         </td>
+         <td>
+          User
+         </td>
+     </tr>
+ </table>
     </header>);
 }
 
